acb31a7f0d63dd6efb14da2e911215b0d3e30276
Honza Horak <hhorak@redhat.com>
2022-09-16 14:08:16 +0200

    [2/3] Add redis v7
    Copy content from 7 to 6, keeping the git history in 7

---
+++ 6/root/usr/share/container-scripts/redis/post-init.sh
@@ -0,0 +1,6 @@
+# This file serves for extending the container image, typically by changing
+# the configuration, loading some data etc.
+
+# Feel free to add content to this file or rewrite it at all.
+# You may also start redis server locally to load some data for example,
+# but do not forget to stop it after it, so it can be restarted after it.
