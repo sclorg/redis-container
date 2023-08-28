b496323808813558f6c039c3642c618aced4ab61
Petr "Stone" Hracek <phracek@redhat.com>
2023-08-28 15:58:36 +0200

    Fixes: #128 Add support for adding bind to /etc/redis.conf by BIND_ADDRESS variable
    Closes #128
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- 6/root/usr/share/container-scripts/redis/README.md
+++ 6/root/usr/share/container-scripts/redis/README.md
@@ -57,6 +57,9 @@ Environment variables and volumes
 **`REDIS_PASSWORD`**  
        Password for the server access
 
+**`BIND_ADDRESS`**
+       Configuration redis to listen on this address
+
 
 You can also set the following mount points by passing the `-v /host:/container:Z` flag to podman.
 
--- 6/root/usr/share/container-scripts/redis/post-init.sh
+++ 6/root/usr/share/container-scripts/redis/post-init.sh
@@ -4,3 +4,8 @@
 # Feel free to add content to this file or rewrite it at all.
 # You may also start redis server locally to load some data for example,
 # but do not forget to stop it after it, so it can be restarted after it.
+
+source "${CONTAINER_SCRIPTS_PATH}/common.sh"
+set -eu
+
+setup_bind_address
