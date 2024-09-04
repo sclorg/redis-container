f015fd0f4ecdbb52b67eca406dc614f8f8f999cd
Petr "Stone" Hracek <phracek@redhat.com>
2024-09-04 14:14:03 +0200

    Testing Helm charts in redis-container stored in https://github.com/sclorg/helm-charts
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_redis_helm_chart_imagestreams.py
@@ -0,0 +1,62 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmRHELRedisImageStreams:
+
+    def setup_method(self):
+        package_name = "redis-imagestreams"
+        path = test_dir
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/redhat"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("6-el8", "registry.redhat.io/rhel8/redis-6:latest"),
+            ("6-el9", "registry.redhat.io/rhel9/redis-6:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+
+
+class TestHelmCentOSRedisImagestreams:
+    def setup_method(self):
+        package_name = "redis-imagestreams"
+        path = test_dir
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/centos"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("6-el8", "quay.io/sclorg/redis-6-c8s:latest"),
+            ("6-el9", "quay.io/sclorg/redis-6-c9s:latest"),
+            ("7-el9", "quay.io/sclorg/redis-7-c9s:latest"),
+        ]
+    )
+    def test_package_imagestream(self, version, registry):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
