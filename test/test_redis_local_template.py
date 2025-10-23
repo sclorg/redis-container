e47df6ec502d94fc979352ec603453ac5ae7aad6
Petr "Stone" Hracek <phracek@redhat.com>
2025-10-23 17:21:38 +0200

    Replace definitions for each OpenShift tests and move it to `conftest.py`
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_redis_local_template.py
+++ test/test_redis_local_template.py
@@ -1,25 +1,14 @@
-import os
-import sys
-
 import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
-from container_ci_suite.utils import check_variables
 
-if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
-    sys.exit(1)
-
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-OS = os.getenv("TARGET")
+from conftest import VARS
 
 
 class TestRedisDeployTemplate:
 
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="redis", version=VERSION)
+        self.oc_api = OpenShiftAPI(pod_name_prefix="redis", version=VARS.VERSION)
 
     def teardown_method(self):
         self.oc_api.delete_project()
@@ -32,21 +21,20 @@ class TestRedisDeployTemplate:
         ]
     )
     def test_redist_template_inside_cluster(self, template):
-        short_version = VERSION.replace(".", "")
         assert self.oc_api.deploy_template_with_image(
-            image_name=IMAGE_NAME,
+            image_name=VARS.IMAGE_NAME,
             template=f"examples/{template}",
             name_in_template="redis",
             openshift_args=[
-                f"REDIS_VERSION={VERSION}",
+                f"REDIS_VERSION={VARS.VERSION}",
                 f"DATABASE_SERVICE_NAME={self.oc_api.pod_name_prefix}",
-                f"REDIS_PASSWORD=testp"
+                "REDIS_PASSWORD=testp"
             ]
         )
 
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
         assert self.oc_api.check_command_internal(
-            image_name=f"registry.redhat.io/{OS}/redis-{short_version}",
+            image_name=f"registry.redhat.io/{VARS.OS}/redis-{VARS.VERSION}",
             service_name=self.oc_api.pod_name_prefix,
             cmd="timeout 15 redis-cli -h <IP> -a testp ping",
             expected_output="PONG"
--- test/test_redis_shared_helm_template.py
+++ test/test_redis_shared_helm_template.py
@@ -1,31 +1,18 @@
-import os
-
-import pytest
-from pathlib import Path
-
 from container_ci_suite.helm import HelmChartsAPI
 
-test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-BRANCH_TO_TEST = "master"
-OS = os.getenv("OS")
-
-TAGS = {
-    "rhel8": "-el8",
-    "rhel9": "-el9"
-}
-
-TAG = TAGS.get(OS, None)
+from conftest import VARS
 
 
 class TestHelmRedisPersistent:
 
     def setup_method(self):
         package_name = "redhat-redis-persistent"
-        path = test_dir
-        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, shared_cluster=True)
+        self.hc_api = HelmChartsAPI(
+            path=VARS.TEST_DIR,
+            package_name=package_name,
+            tarball_dir=VARS.TEST_DIR,
+            shared_cluster=True
+        )
         self.hc_api.clone_helm_chart_repo(
             repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
             subdir="charts/redhat"
@@ -42,7 +29,7 @@ class TestHelmRedisPersistent:
         self.hc_api.helm_package()
         assert self.hc_api.helm_installation(
             values={
-                "redis_version": f"{VERSION}{TAG}",
+                "redis_version": f"{VARS.VERSION}{VARS.TAG}",
                 "namespace": self.hc_api.namespace
             }
         )
