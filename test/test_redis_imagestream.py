229c7e19d3a95989ba3c549d646b34b5cc4d2104
Petr "Stone" Hracek <phracek@redhat.com>
2025-10-23 17:21:38 +0200

    Replace definitions for each OpenShift tests and move it to `conftest.py`
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_redis_imagestream.py
+++ test/test_redis_imagestream.py
@@ -1,30 +1,14 @@
-import os
-import sys
-
 import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
-from container_ci_suite.utils import check_variables
 
-if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
-    sys.exit(1)
-
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-OS = os.getenv("TARGET")
-TAGS = {
-    "rhel8": "-el8",
-    "rhel9": "-el9"
-}
-TAG = TAGS.get(OS, None)
+from conftest import VARS
 
 
 class TestRedisImagestreamTemplate:
 
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="redis", version=VERSION)
+        self.oc_api = OpenShiftAPI(pod_name_prefix="redis", version=VARS.VERSION)
 
     def teardown_method(self):
         self.oc_api.delete_project()
@@ -37,13 +21,13 @@ class TestRedisImagestreamTemplate:
         ]
     )
     def test_redis_imagestream_template(self, template):
-        os_name = ''.join(i for i in OS if not i.isdigit())
+        os_name = ''.join(i for i in VARS.OS if not i.isdigit())
         assert self.oc_api.deploy_image_stream_template(
             imagestream_file=f"imagestreams/redis-{os_name}.json",
             template_file=f"examples/{template}",
             app_name=self.oc_api.pod_name_prefix,
             openshift_args=[
-                f"REDIS_VERSION={VERSION}{TAG}"
+                f"REDIS_VERSION={VARS.VERSION}{VARS.TAG}"
             ]
         )
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
