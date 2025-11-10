e26e2ed92d9a173f5aaaeb8031544107857c5884
Petr "Stone" Hracek <phracek@redhat.com>
2025-11-10 12:14:29 +0100

    Do not use valkey in tests at all
    use --user 12345 and not --user 100001. It does not make sense
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_container_application.py
+++ test/test_container_application.py
@@ -20,7 +20,7 @@ class TestRedisApplicationContainer:
             ("", "no_pass"),
             ("pass", "no_pass_altuid"),
             ("pass", "no_root_altuid"),
-        ]
+        ],
     )
     def test_run_app_test(self, password, test_name):
         """
@@ -29,10 +29,13 @@ class TestRedisApplicationContainer:
         and with different user and password
         """
         container_arg = f"-e REDIS_PASSWORD={password}" if password else ""
-        container_arg = f"{container_arg} -u 12345" if "altuid" in test_name else container_arg
+        container_arg = (
+            f"{container_arg} -u 12345"
+            if "altuid" in test_name
+            else f"{container_arg} --user=100001"
+        )
         assert self.s2i_app.create_container(
-            cid_file_name=test_name,
-            container_args=f"--user=100001 {container_arg}"
+            cid_file_name=test_name, container_args=container_arg
         )
         # assert ContainerImage.wait_for_cid(cid_file_name=test_name)
         cid = self.s2i_app.get_cid(cid_file_name=test_name)
@@ -43,38 +46,37 @@ class TestRedisApplicationContainer:
         redis_cmd = f"redis-cli -h {cip} {testing_password}"
         # Test with redis-cli returns 'PONG' from the different container
         redis_output = PodmanCLIWrapper.podman_run_command_and_remove(
-            cid_file_name=VARS.IMAGE_NAME,
-            cmd=f"{redis_cmd} ping"
+            cid_file_name=VARS.IMAGE_NAME, cmd=f"{redis_cmd} ping"
         ).strip()
         assert "PONG" in redis_output, f"The command {redis_cmd} should return PONG"
         # The password '_foo' has to fail. It was not initiated
         assert not PodmanCLIWrapper.podman_run_command_and_remove(
             cid_file_name=VARS.IMAGE_NAME,
             cmd=f"{redis_cmd}_foo ping",
-            return_output=False
-        ), "The command -e REDIS_PASSWORD=\"pass_foo\" has to fail"
+            return_output=False,
+        ), 'The command -e REDIS_PASSWORD="pass_foo" has to fail'
         # The redis-cli should return PONG from the running container
         redis_output = PodmanCLIWrapper.podman_exec_shell_command(
-            cid_file_name=cid,
-            cmd=f"{redis_cmd} ping"
+            cid_file_name=cid, cmd=f"{redis_cmd} ping"
         )
         assert "PONG" in redis_output, "Local access FAILED"
         # The redis-cli set value 'a' to 1
-        assert PodmanCLIWrapper.podman_exec_shell_command(
-            cid_file_name=cid,
-            cmd=f"{redis_cmd} set a 1",
-            return_output=False
-        ) == 0
+        assert (
+            PodmanCLIWrapper.podman_exec_shell_command(
+                cid_file_name=cid, cmd=f"{redis_cmd} set a 1", return_output=False
+            )
+            == 0
+        )
         # The redis-cli set value 'b' to 2
-        assert PodmanCLIWrapper.podman_exec_shell_command(
-            cid_file_name=cid,
-            cmd=f"{redis_cmd} set b 2",
-            return_output=False
-        ) == 0
+        assert (
+            PodmanCLIWrapper.podman_exec_shell_command(
+                cid_file_name=cid, cmd=f"{redis_cmd} set b 2", return_output=False
+            )
+            == 0
+        )
         # Checks if value b is set to '2'
         redis_get_value = PodmanCLIWrapper.podman_exec_shell_command(
-            cid_file_name=cid,
-            cmd=f"{redis_cmd} get b 2>/dev/null"
+            cid_file_name=cid, cmd=f"{redis_cmd} get b 2>/dev/null"
         ).strip()
         assert int(redis_get_value) == 2
 
@@ -83,7 +85,7 @@ class TestRedisApplicationContainer:
         [
             "",
             "-e BIND_ADDRESS=127.0.0.1",
-        ]
+        ],
     )
     def test_bind_address(self, bind_address):
         """
@@ -94,34 +96,38 @@ class TestRedisApplicationContainer:
         cid_file_name = "bind_address"
         assert self.s2i_app.create_container(
             cid_file_name=cid_file_name,
-            container_args=f"--user=100001 -e REDIS_PASSWORD=pass {bind_address}"
+            container_args=f"--user=100001 -e REDIS_PASSWORD=pass {bind_address}",
         )
         cid = self.s2i_app.get_cid(cid_file_name=cid_file_name)
         assert cid
         cip = self.s2i_app.get_cip(cid_file_name=cid_file_name)
         assert cip
-        assert PodmanCLIWrapper.podman_exec_shell_command(
-            cid_file_name=cid,
-            cmd="test -f ${REDIS_CONF}",
-            return_output=False
-        ) == 0
+        assert (
+            PodmanCLIWrapper.podman_exec_shell_command(
+                cid_file_name=cid, cmd="test -f ${REDIS_CONF}", return_output=False
+            )
+            == 0
+        )
         if bind_address:
-            assert PodmanCLIWrapper.podman_exec_shell_command(
-                cid_file_name=cid,
-                cmd="grep \"^bind 127.0.0.1\" ${REDIS_CONF}",
-                return_output=False
-            ) == 0
-            # Quit test suite. valkey container will not response. bind is set to localhost
+            assert (
+                PodmanCLIWrapper.podman_exec_shell_command(
+                    cid_file_name=cid,
+                    cmd='grep "^bind 127.0.0.1" ${REDIS_CONF}',
+                    return_output=False,
+                )
+                == 0
+            )
         else:
             # Check that REDIS_CONF really does NOT contain 'bind 127.0.0.1'
             assert not PodmanCLIWrapper.podman_exec_shell_command(
                 cid_file_name=cid,
-                cmd="grep \"^bind 127.0.0.1\" ${REDIS_CONF}",
-                return_output=False
+                cmd='grep "^bind 127.0.0.1" ${REDIS_CONF}',
+                return_output=False,
             )
             # Checks if redis-cli returns PONG
             redis_return_value = PodmanCLIWrapper.podman_run_command_and_remove(
-                cid_file_name=VARS.IMAGE_NAME,
-                cmd=f"redis-cli -h {cip} -a pass ping"
+                cid_file_name=VARS.IMAGE_NAME, cmd=f"redis-cli -h {cip} -a pass ping"
             ).strip()
-            assert "PONG" in redis_return_value, f"redis-cli -h {cip} -a pass ping has to return PONG"
+            assert "PONG" in redis_return_value, (
+                f"redis-cli -h {cip} -a pass ping has to return PONG"
+            )
